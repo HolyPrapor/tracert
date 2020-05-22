f41cc93ba708c5b734605219949bde605b5e6beb
Zeliboba <tolya12345w@gmail.com>
2020-05-22 17:56:27 +0500

    Finished traceroute

--- icmp.py
+++ icmp.py
@@ -4,114 +4,101 @@ from enum import IntEnum
 
 
 class IcmpType(IntEnum):
-    ECHO_REPLY = 1
+    ECHO_REPLY = 0
     ECHO_REQUEST = 8
     TTL_EXCEEDED = 11
 
 
-def extract_data_from_icmp_packet(packet):
-    version = (packet[0] & 0xf0) // 16
-    if version == 4:
-        header_length = (packet[0] & 0x0f) * 4
-        if packet[9] == 1:
-            return packet[header_length:]
-    return None
-
-
-def with_inserted_checksum(packet, offset=2):
-    checksum = sum(int.from_bytes(packet[i:i + 2], 'big')
-                   for i in range(0, len(packet), 2))
-    while checksum > 0xffff:
-        checksum = (checksum & 0xffff) + (checksum >> 16)
-    return (packet[:offset] + int.to_bytes(0xffff - checksum, 2, 'big')
-            + packet[offset + 2:])
-
-
-class IcmpPacket:
-    def __init__(self, **kwargs):
-        self.type = None
-        self.code = None
-        self.id = None
-        self.sequence_number = None
-        self.child = None
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-        if self.type == IcmpType.ECHO_REQUEST or IcmpType.ECHO_REPLY:
+class ICMPPacket:
+    def __init__(self, type_: int, code: int, other: bytes = None, **kwargs):
+        self.type = type_
+        self.code = code
+        self.other = other
+        if (self.type == IcmpType.ECHO_REQUEST
+                or self.type == IcmpType.ECHO_REPLY):
             self.id = random.randint(0, 2 ** 16 - 1)
             self.sequence_number = random.randint(0, 2 ** 16 - 1)
         if self.type == IcmpType.TTL_EXCEEDED:
             self.child = None
 
+        for key, item in kwargs.items():
+            setattr(self, key, item)
+
+    @classmethod
+    def extract_icmp_data(cls, data):
+        version = (data[0] & 0xf0) // 16
+        if version == 4:
+            header_length = (data[0] & 0x0f) * 4
+            if data[9] == 1:
+                return data[header_length:]
+        return None
+
     @classmethod
     def from_bytes(cls, data):
         if data[0] == IcmpType.TTL_EXCEEDED:
-            child = IcmpPacket.from_bytes(
-                extract_data_from_icmp_packet(data[8:]))
-            return IcmpPacket(type=data[0], code=data[1], child=child)
+            child = ICMPPacket.from_bytes(
+                ICMPPacket.extract_icmp_data(data[8:]))
+            return ICMPPacket(data[0], data[1], child=child)
         if data[0] == IcmpType.ECHO_REPLY or data[0] == IcmpType.ECHO_REQUEST:
-            return IcmpPacket(type=data[0], code=data[1],
+            return ICMPPacket(data[0], data[1],
                               id=int.from_bytes(data[4:6], 'big'),
                               sequence_number=int.from_bytes(data[6:8], 'big'))
 
     def __bytes__(self):
-        byte_view = bytes([self.type, self.code]) + b'\0\0'
+        result = bytes([self.type, self.code]) + b'\0\0'
         if self.type == IcmpType.ECHO_REQUEST:
-            byte_view += (int.to_bytes(self.id, 2, 'big') +
-                          int.to_bytes(self.sequence_number, 2, 'big'))
-        return with_inserted_checksum(byte_view)
+            result += (int.to_bytes(self.id, 2, 'big') +
+                       int.to_bytes(self.sequence_number, 2, 'big'))
+        return ICMPPacket.with_inserted_checksum(result)
 
-    def __str__(self):
-        return repr(self)
+    @classmethod
+    def with_inserted_checksum(cls, packet):
+        checksum = sum([int.from_bytes(packet[_:_ + 2], "big") for _ in
+                        range(0, len(packet), 2)])
+        while checksum > 0xffff:
+            checksum = (checksum & 0xffff) + (checksum >> 16)
+        return (packet[:2] + int.to_bytes(0xffff - checksum, 2, 'big')
+                + packet[4:])
 
-    def __repr__(self):
-        args = [f'ICMP({self.type}, {self.code}']
-        if self.type == IcmpType.ECHO_REPLY or self.type == IcmpType.ECHO_REQUEST:
-            args.append(f'id={self.id}, sequence_number={self.sequence_number}')
-        if self.type == IcmpType.TTL_EXCEEDED:
-            args.append(f'child={repr(self.child)}')
-        return ',\n'.join(args) + ')'
-
-    def is_needed_node(self, other):
+    def is_answer(self, other):
         if other is None:
             return False
         if other.type == IcmpType.ECHO_REPLY:
-            return (self.id == other.id and
-                    self.sequence_number == other.sequence_number)
+            return (self.id == other.id
+                    and self.sequence_number == other.sequence_number)
         if other.type == IcmpType.TTL_EXCEEDED:
             return (other.child is not None and self.id == other.child.id and
                     self.sequence_number == other.child.sequence_number)
 
 
-def icmp_sniffer(interface_ip, timeout):
+def get_icmp_listener(interf_ip, timeout=0):
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    sock.bind((interface_ip, 0))
+    sock.bind((interf_ip, 0))
     sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
     sock.settimeout(timeout)
     return sock
 
 
-def get_trace(interface_ip, destination_ip, depth, step_timeout):
-    sock = icmp_sniffer(interface_ip, step_timeout)
-
-    icmp_packet = IcmpPacket(type=IcmpType.ECHO_REQUEST, code=0)
+def get_trace(inter_ip, dest_ip, depth=15, timeout_for_step=2):
+    sock = get_icmp_listener(inter_ip, timeout_for_step)
+    packet = ICMPPacket(IcmpType.ECHO_REQUEST, 0,
+                        id=random.randint(0, 2 ** 16), sequence_number=15)
     for i in range(1, depth + 1):
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, i)
-        icmp_packet.sequence_number += 1
-        sock.sendto(bytes(icmp_packet), (destination_ip, 0))
+        packet.sequence_number += 1
+        sock.sendto(bytes(packet), (dest_ip, 0))
         result = None
         while not result:
             try:
-                data_, addr = sock.recvfrom(2 ** 16 - 1)
-                response_packet = IcmpPacket.from_bytes(
-                    extract_data_from_icmp_packet(data_))
-                if addr[0] == interface_ip or not icmp_packet.is_needed_node(
-                        response_packet):
+                response_data, addr = sock.recvfrom(65535)
+                response = ICMPPacket.from_bytes(
+                    ICMPPacket.extract_icmp_data(response_data))
+                if addr[0] == inter_ip or not packet.is_answer(response):
                     continue
                 result = addr[0]
             except socket.error:
-
-                result = "BAD"
+                result = '*'
+            break
         yield result
-        if result == destination_ip:
+        if result == dest_ip:
             break
--- tracert.py
+++ tracert.py
@@ -14,11 +14,10 @@ def is_root_user():
         except OSError:
             return False
         return True
-
     return 'SUDO_USER' in os.environ and os.geteuid() == 0
 
 
-def check_correct_ip(ip: str):
+def check_correct_ip(ip):
     splitted = ip.split('.')
     if len(splitted) != 4:
         return False
@@ -26,60 +25,55 @@ def check_correct_ip(ip: str):
         try:
             if int(a) > 255 or int(a) < 0:
                 return False
-        except Exception:
+        except:
             return False
     return True
 
 
-def parse_args(args, host_ips):
-    available_hosts = 'Available interface ips:\r\n' + "\r\n".join(map(lambda x: " - " + x, host_ips))
-    parser = argparse.ArgumentParser(description="Simple TraceRoute with additional information from WhoIs service.\n"
-                                                 f"It works only from root user.")
-
-    parser.add_argument('interface_ip', action='store', help='ip address of network interface, that will '
-                                                             'be listened. Format - xxx.xxx.xxx.xxx')
-    parser.add_argument('dest_ip', action='store', help='Destination ip. Format - xxx.xxx.xxx.xxx\nIt also '
-                                                        'can be dns address')
-    parser.add_argument('depth', action='store', type=int, nargs='?', default=15, help='Max TTL')
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Simple TraceRoute with additional"
+                                                 " information from WhoIs service.")
+    parser.add_argument('destination_ip', action='store')
+    parser.add_argument('depth', action='store', type=int, nargs='?',
+                        default=15, help='Max TTL')
 
     parsed = parser.parse_args(args)
 
-    if not check_correct_ip(parsed.interface_ip) or parsed.interface_ip not in host_ips:
-        sys.stderr.write("Enter correct interface ip. Available Host IPs:\n" + available_hosts)
-        exit(2)
-
     if parsed.depth < 1:
-        sys.stderr.write("Enter correct depth")
+        print("Depth is incorrect!")
         exit(2)
 
     return parser.parse_args(args)
 
 
 def main(args):
-    host_ips = socket.gethostbyname_ex(socket.gethostname())[2]
-    args = parse_args(args[1:], host_ips)
+    interface_ips = socket.gethostbyname_ex(socket.gethostname())[2]
+    if not interface_ips:
+        print("No available IP interface was found!")
+        exit(3)
+    args = parse_args(args[1:])
     if not is_root_user():
-        print("You should run this script under root user")
-        return
+        print("You should be under root to perform this operation!")
+        exit(4)
 
     try:
-        args.dest_ip = socket.gethostbyname(args.dest_ip)
+        args.destination_ip = socket.gethostbyname(args.destination_ip)
     except socket.error:
-        print(f"Address {args.dest_ip} is not correct. Please enter the correct address")
-        exit(2)
+        print(f"Address {args.destination_ip} is incorrect!")
+        exit(5)
 
-    for counter, i in enumerate(icmp.get_trace(args.interface_ip, args.dest_ip, args.depth, 2)):
-        to_print = [str(counter + 1), '. ', i, '\r\n']
-        if i != 'BAD':
-            if whois.is_ip_local(i):
-                to_print.append('local')
+    for counter, address in enumerate(icmp.get_trace(interface_ips[0],
+                                                     args.destination_ip,
+                                                     args.depth, 2)):
+        print(f"{counter + 1}. {address}\r\n", end='')
+        if address != '*':
+            if whois.is_ip_local(address):
+                print('Local', end='')
             else:
-                data = whois.get_whois_info(i)
-                data = filter(lambda x: x, data)
-                to_print.append(', '.join(data))
-            to_print.append('\r\n')
-        to_print.append('\r\n')
-        print(''.join(to_print), end='')
+                print(', '.join(filter(lambda x: x,
+                                       whois.get_whois_info(address))), end='')
+            print('\r\n', end='')
+        print('\r\n')
 
 
 if __name__ == "__main__":
